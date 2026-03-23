bdf08c15892d659207a19336cd74ec605f803d41
bvrt3kk <danusek12345@gmail.com>
2026-03-23 14:18:24 +0100

    inna metoda dodania listy

--- app/src/main/java/com/example/listy/MainActivity.java
+++ app/src/main/java/com/example/listy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listy;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> jezykiArrayList;
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        jezykiArrayList = new ArrayList<>();
+        jezykiArrayList.add("Perl");
+        jezykiArrayList.add("Ruby");
+        jezykiArrayList.add("Kotlin");
+        jezykiArrayList.add("Go");
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, jezykiArrayList);
+        listView = findViewById(R.id.listViewJezyki);
+        listView.setAdapter(arrayAdapter);
     }
 }
